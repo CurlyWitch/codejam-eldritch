b72fdd47d5ba503ab705cc12b84471249c4bdf55
CurlyWitch <yuliya.skrygan@gmail.com>
2022-08-25 22:34:10 +0300

    feat: add difficulties, switch active ancient

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 import AncientsData from '/data/ancients.js'
+import Difficulties from '/data/difficulties.js'
 
 function init() {
     initAncient();
+    Array.from(document.getElementsByClassName('ancient-card')).forEach(e => e.addEventListener('click', clickAncient));
 }
 
 init();
@@ -12,6 +14,23 @@ function initAncient() {
         div.classList.add("ancient-card");
         div.style.backgroundImage = `url('${e.cardFace}')`;
         document.querySelector('.ancients-container').appendChild(div);
-
     })
+}
+
+function initDifficulties() {
+    document.querySelector('.difficulty-container').innerHTML = '';
+    Object.values(Difficulties).forEach(e => {
+        const div =  document.createElement("div");
+        div.classList.add("difficulty");
+        div.id = e.id;
+        div.textContent = e.name;  
+        document.querySelector('.difficulty-container').appendChild(div);
+    })
+}
+
+
+function clickAncient(event) {
+    Array.from(document.getElementsByClassName('ancient-card')).forEach(e => e.classList.remove("active"));
+    event.target.classList.add("active");
+    initDifficulties();
 }
